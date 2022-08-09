e2be60d5da77e9b0530beb5e32cd122abcf70408
Snickbit <snickbit@users.noreply.github.com>
2022-08-08 21:07:05 -0400

    feat(semantic-release): add github pull requests by default

---
+++ packages/semantic-release/src/plugins/github-pullrequest.ts
@@ -0,0 +1,4 @@
+export default ['semantic-release-github-pullrequest', {
+	assets: ['CHANGELOG.md'],
+	baseRef: 'main'
+}]
--- packages/semantic-release/src/plugins/github.ts
+++ packages/semantic-release/src/plugins/github.ts
@@ -1,1 +1,1 @@
-export default ['@semantic-release/github', {assets: 'pack/*.tgz'}]
+export default ['@semantic-release/github', {assets: ['pack/*.tgz', 'CHANGELOG.md']}]
