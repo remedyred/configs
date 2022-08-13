22f53599b9e5c76e7b0040f12c6f1f541f5c7060
Snickbit <snickbit@users.noreply.github.com>
2022-08-13 17:23:14 -0400

    test(eslint): improve tests and add snapshots

--- packages/eslint-config/__tests__/index.test.ts
+++ packages/eslint-config/__tests__/index.test.ts
@@ -1,35 +1,77 @@
-import {ESLint} from 'eslint'
+import {ESLint, Linter} from 'eslint'
+import {fileExists} from '@snickbit/node-utilities'
 import {unixify} from 'fast-glob/out/utils/path'
 import fg from 'fast-glob'
-import eslintConfig from '../src/configs'
-import vueConfig from '../src/configs/vue'
 import path from 'path'
 
-export async function testFile(fileToTest: string) {
-	const ext = path.extname(fileToTest)
-	const baseConfig = ext === '.vue' ? vueConfig : eslintConfig
-
-	// 1. Create an instance.
-	const eslint = new ESLint({
-		baseConfig,
-		useEslintrc: false,
-		cache: false
-	})
-
-	// 2. Lint files.
-	const results = await eslint.lintFiles([fileToTest])
-
-	// 3. Report results.
-	return results.pop()
+const CONFIG_PATH = path.join(__dirname, '..', 'dist', 'configs')
+if (!fileExists(CONFIG_PATH)) {
+	throw new Error(`Config path ${CONFIG_PATH} does not exist. Did you forget to run "pnpm build"?`)
 }
 
 const MOCK_FILES_PATH = `${unixify(__dirname)}/../__fixtures__`
-const MOCK_FILES = fg.sync('**/*', {cwd: MOCK_FILES_PATH})
+const MOCK_FILES = fg.sync('**/*', {cwd: MOCK_FILES_PATH, ignore: ['.eslintrc.json']})
+
+export class MockLint {
+	eslint: ESLint
+
+	constructor(config?: Linter.Config | string) {
+		this.setConfig(config)
+	}
+
+	setConfig(config?: Linter.Config | string) {
+		const options: ESLint.Options = {
+			cwd: MOCK_FILES_PATH,
+			cache: false,
+			ignore: false
+		}
+
+		if (config) {
+			if (typeof config === 'string') {
+				if (config.endsWith('.js') && fileExists(config)) {
+					options.overrideConfigFile = config
+				} else if (fileExists(`${config}.js`)) {
+					options.overrideConfigFile = `${config}.js`
+				}
+			} else {
+				options.overrideConfig = config
+			}
+		}
+
+		if (!options.overrideConfig && !options.overrideConfigFile) {
+			options.overrideConfigFile = `${CONFIG_PATH}/all.js`
+		}
+
+		this.eslint = new ESLint(options)
+	}
+
+	async getConfig(file: string): Promise<Linter.Config> {
+		return this.eslint.calculateConfigForFile(file)
+	}
+
+	async lintFile(file: string): Promise<ESLint.LintResult[]> {
+		return this.eslint.lintFiles([file])
+	}
+
+	async lintText(text: string, file: string): Promise<ESLint.LintResult[]> {
+		return this.eslint.lintText(text, {filePath: file})
+	}
+}
 
 describe('Validate ESLint configs', () => {
-	it.each(MOCK_FILES)('should pass ESLint for %s', file => {
-		expect(() => {
-			expect(testFile(file)).resolves.toBeUndefined()
-		}).not.toThrow()
+	describe.each(MOCK_FILES)('.add(Validate ESLint configs for %s)', file => {
+		const linter = new MockLint(CONFIG_PATH)
+
+		it('should pass ESLint', async () => {
+			expect(linter).toBeInstanceOf(MockLint)
+		})
+
+		it('should return the proper config', async () => {
+			expect(linter.getConfig(file)).resolves.toMatchSnapshot()
+		})
+
+		it('should return the proper lint results', async () => {
+			expect(linter.lintFile(file)).resolves.toMatchSnapshot()
+		})
 	})
 })
