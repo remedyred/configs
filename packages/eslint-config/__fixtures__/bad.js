d9990197ba5765cafdd4ce80da70257baa670bd5
Snickbit <snickbit@users.noreply.github.com>
2022-08-20 11:47:52 -0400

    chore(eslint): add ide flags to prevent errors in fixture file

--- packages/eslint-config/__fixtures__/bad.js
+++ packages/eslint-config/__fixtures__/bad.js
@@ -1,1 +1,4 @@
-function sample(arr, req) {	arr = arr.sort(() => {		return 0.5 - Math.random()	});																				let i = 0,																					array = [];																				while (i < req) {		array.push(arr[i])		++i	}	return array}
+// noinspection JSUnusedGlobalSymbols,JSAnnotator
+// @formatter:off
+
+function sample(arr, req) {	arr = arr.sort(() => {		return 0.5 - Math.random()	});																				let i = 0,																					array = [];																				while (i < req) {		array.push(arr[i]);		++i	}	return array}
