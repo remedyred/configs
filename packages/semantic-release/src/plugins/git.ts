3a804b25e1fbee03febe921c1edbdea2c924f5db
Snickbit <snickbit@users.noreply.github.com>
2022-08-21 20:54:25 -0400

    fix(semantic-release): update package.json in the repo

--- packages/semantic-release/src/plugins/git.ts
+++ packages/semantic-release/src/plugins/git.ts
@@ -1,1 +1,7 @@
-export default ['@semantic-release/git', {message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'} ]
+export default [
+	'@semantic-release/git',
+	{
+		assets: ['package.json'],
+		message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+	}
+]
