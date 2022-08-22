c77dac473b190f4a4a92327f8fd0081d7a0770dd
Snickbit <snickbit@users.noreply.github.com>
2022-08-21 22:38:50 -0400

    fix(semantic-release): add docs and readme to git add

--- packages/semantic-release/src/plugins/git.ts
+++ packages/semantic-release/src/plugins/git.ts
@@ -1,7 +1,7 @@
 export default [
 	'@semantic-release/git',
 	{
-		assets: ['package.json'],
+		assets: ['package.json', 'docs', 'README.md'],
 		message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 	}
 ]
