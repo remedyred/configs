32a406ab24856b164c814cf8770ecd9e3f8e7ab9
Snickbit <snickbit@users.noreply.github.com>
2022-08-24 17:43:52 -0400

    style(semantic-release): reformat code

--- packages/semantic-release/src/plugins/changelog.ts
+++ packages/semantic-release/src/plugins/changelog.ts
@@ -1,1 +1,9 @@
-export default ['@semantic-release/changelog', {changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.'} ]
+export default [
+	'@semantic-release/changelog',
+	{
+		changelogTitle: `# Changelog
+
+All notable changes to this project will be documented in this file. See
+[Conventional Commits](https://conventionalcommits.org) for commit guidelines.`
+	}
+]
