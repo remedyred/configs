73ada3cc467f238876cddeccce1935f6f9e77739
Snickbit <snickbit@users.noreply.github.com>
2022-08-24 18:09:57 -0400

    fix: semantic release plugin errors (#61)

--- packages/semantic-release/src/plugins/semantic-release-conventional-commits.ts
+++ packages/semantic-release/src/plugins/semantic-release-conventional-commits.ts
@@ -1,1 +1,16 @@
-export default 'semantic-release-conventional-commits'
+/* eslint-disable object-curly-newline */
+export default {
+	majorTypes: ['breaking'],
+	minorTypes: ['feat', 'minor'],
+	patchTypes: [
+		'patch',
+		'chore',
+		'docs',
+		'fix',
+		'refactor',
+		'style',
+		'test'
+	],
+	mergePattern: /^Merge pull request #(\d+) from (.*)$/,
+	mergeCorrespondence: ['id', 'source']
+}
