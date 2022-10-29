c89f7df5d738141171ba548774c1dfd9d4999c1a
Snickbit <snickbit@users.noreply.github.com>
2022-10-29 12:10:22 -0400

    build: remove per-package tsconfig files
    [skip ci]

--- packages/eslint-config/__fixtures__/bad.js
+++ packages/eslint-config/__fixtures__/bad.js
@@ -1,4 +1,5 @@
 // noinspection JSUnusedGlobalSymbols,JSAnnotator
 // @formatter:off
+// @ts-nocheck
 
 function sample(arr, req) {	arr = arr.sort(() => {		return 0.5 - Math.random()	});																				let i = 0,																					array = [];																				while (i < req) {		array.push(arr[i]);		++i	}	return array}
