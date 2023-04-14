8a8c108303398d65e300ab8e701e270293479576
Snickbit <snickbit@users.noreply.github.com>
2023-04-14 17:24:53 -0400

    fix: improve json linting and formatting

--- packages/eslint-config/src/plugins/json/rules.ts
+++ packages/eslint-config/src/plugins/json/rules.ts
@@ -1,6 +1,7 @@
 export = {
 	rules: {
 		'json-files/require-unique-dependency-names': 'error',
-		'json-files/sort-package-json': 'error'
+		'json-files/sort-package-json': 'error',
+		'jsonc/auto': 'error'
 	}
 }
