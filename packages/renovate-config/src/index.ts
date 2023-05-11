1a52c9dff6894aff463666577830212291240a67
Snickbit <snickbit@users.noreply.github.com>
2023-05-11 16:39:49 -0400

    fix(renovate): proper cli import

--- packages/renovate-config/src/index.ts
+++ packages/renovate-config/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import {defaultConfig} from './common'
+import {cli} from '@snickbit/node-cli'
 import * as actions from './actions'
-import cli from '@snickbit/node-cli'
 
 cli()
 	.name('builder')
